185a1cbfa49ef698c49536cf4152ee3ef8afa7e3
SARA LEE <saragabyleegon2@mail.com>
2024-08-29 23:27:41 -0500

    Variable local (Dentro de Metodo)

--- Laboratorio2/Laboratorio2/Program.cs
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -2,6 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Variable local dentro de Metodo, Esta variable (Variablelocal) solo funcionara dentro de este metodo
+
+        public void Sumar (int Valor1, int Valor2)
+        {
+            int Variablelocal = Valor1 + Valor2;
+            Console.WriteLine(Variablelocal);
+        }
+
     }
 }
