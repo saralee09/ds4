f731a8562295fca1cbf52ad9c985e3105883b66a
SARA LEE <saragabyleegon2@mail.com>
2024-09-02 18:52:41 -0500

    ejemplo #1

--- Laboratorio3/Laboratorio3/Program.cs
+++ Laboratorio3/Laboratorio3/Program.cs
@@ -2,6 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int primerNumero, segundoNumero, suma;
+
+        Console Write("Introduce el primer numero: *);
+        primerNumero = Convert.ToInt32(Console.ReadLineO);
+
+        Console Write("Introduce el segundo numero: *):
+        segundoNumero = Convert.ToInt32(Console ReadLineO);
+
+        suma = primerNumero + segundoNumero;
+        Console WriteLine("La suma de (0) y (1) es (2)*, primerNumero, segundoNumero, suma);
     }
 }
