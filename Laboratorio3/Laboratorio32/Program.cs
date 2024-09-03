dcc9a4819b1e7fd249d888d5ce1227a5e0433501
SARA LEE <saragabyleegon2@mail.com>
2024-09-02 19:44:22 -0500

    Laboratorio 3.2

--- Laboratorio3/Laboratorio32/Program.cs
+++ Laboratorio3/Laboratorio32/Program.cs
@@ -1,7 +1,35 @@
-﻿internal class Program
+﻿
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double radio, ans;
+
+        Console.WriteLine("Introduce el radio del circulo: ");
+        radio = Convert.ToDouble(Console.ReadLine());
+
+        CalculosMatematicos calculos = new CalculosMatematicos();
+
+        ans = calculos.CalculoArea(radio, 3.1416);
+
+        Console.WriteLine("El área del círculo con radio {0} es {1}", radio, ans);
     }
 }
+
+internal class CalculosMatematicos
+{
+    public int Calcular(int a, int b)
+    {
+        int suma = a + b;
+        int resta = a - b;
+        return suma * resta;
+    }
+
+    public double CalculoArea(double r, double pi)
+    {
+        double area = pi * (r * r);
+        return area;
+    }
+}
+
+
