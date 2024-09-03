87318e9393eb3530a955f804aa79035a4f4a57fe
SARA LEE <saragabyleegon2@mail.com>
2024-09-02 19:05:22 -0500

    Push

--- Laboratorio3/Laboratorio3/Program.cs
+++ Laboratorio3/Laboratorio3/Program.cs
@@ -4,13 +4,14 @@
     {
         int primerNumero, segundoNumero, suma;
 
-        Console Write("Introduce el primer numero: *);
+        static Console WriteLine("Introduce el primer numero: *);
         primerNumero = Convert.ToInt32(Console.ReadLineO);
 
-        Console Write("Introduce el segundo numero: *):
+        static Console Write("Introduce el segundo numero: *):
         segundoNumero = Convert.ToInt32(Console ReadLineO);
 
         suma = primerNumero + segundoNumero;
-        Console WriteLine("La suma de (0) y (1) es (2)*, primerNumero, segundoNumero, suma);
+
+        static Console WriteLine("La suma de (0) y (1) es (2)*, primerNumero, segundoNumero, suma);
     }
 }
