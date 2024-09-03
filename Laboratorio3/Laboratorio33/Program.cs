5c302880d4d692de06305db557f9c920026cd5a6
SARA LEE <saragabyleegon2@mail.com>
2024-09-02 20:01:02 -0500

    Laboratorio 3.3 Completo!

---
+++ Laboratorio3/Laboratorio33/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Transactions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double numa, numb, ans;
+
+        Console.WriteLine("Introduce la medida de un lado del rectángulo, lado A: ");
+        numa = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Introduce la medida de otro lado del rectángulo, lado B: ");
+        numb = Convert.ToDouble(Console.ReadLine());
+
+        CalculosMatematicos calculos = new CalculosMatematicos();
+
+        ans = calculos.CalculoPerimetro(numa, numb);
+
+        Console.WriteLine("El perímetro del rectángulo con lados {0} y {1} es {2}", numa, numb, ans);
+    }
+}
+
+internal class CalculosMatematicos
+{
+    public double CalculoArea(double r, double pi)
+    {
+        double area = pi * (r * r);
+        return area;
+    }
+
+    public double CalculoPerimetro(double A, double B)
+    {
+        double perimetro = 2 * (A + B);
+        return perimetro;
+    }
+}
