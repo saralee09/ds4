71d8b82b5b53c57ac711c29907983fd7363b4416
SARA LEE <saragabyleegon2@mail.com>
2024-09-11 19:56:02 -0500

    Lab 4 , Completo!

--- Laboratorio 4/Laboratorio42/Program.cs
+++ Laboratorio 4/Laboratorio42/Program.cs
@@ -1,7 +1,19 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int n, x;
+        string linea;
+        Console.Write("Ingrese el valor de n:");
+        linea = Console.ReadLine();
+        n = int.Parse(linea);
+        x = 1;
+        while (x <= n)
+        {
+            Console.Write(x);
+            Console.Write(" , ");
+            x = x + 1;
+        }
+        Console.ReadKey();
     }
 }
