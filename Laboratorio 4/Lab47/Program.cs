6512e9e64ee17e5debcdd644309200a3320906d6
SARA LEE <saragabyleegon2@mail.com>
2024-09-15 21:25:26 -0500

    Lab 4.7

---
+++ Laboratorio 4/Lab47/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int numeroUno = 70;
+        double numeroDos = 67.89;
+        double numeroTres = 67.89;
+        Console.WriteLine(Suma(numeroUno, numeroDos));
+        Console.WriteLine(Suma(numeroUno, numeroDos, numeroTres));
+        //compare y analice las salidas de las 2 sumas realizadas
+    }
+    static double Suma(int x, double y, double z = 0)
+    {
+        return x + y + z;
+    }
+}
