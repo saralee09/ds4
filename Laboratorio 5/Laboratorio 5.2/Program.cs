d961c3d7ddbe212b805643ab4731e4c6218cc6aa
SARA LEE <saragabyleegon2@mail.com>
2024-09-16 19:25:13 -0500

    Laboratorio 5.2

---
+++ Laboratorio 5/Laboratorio 5.2/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+        private int[,] mat;
+        public void Ingresar()
+        {
+            mat = new int[3, 4];
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    Console.Write("Ingrese posicion [" + (f + 1) + "," + (c + 1) + "]: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    mat[f, c] = int.Parse(linea);
+                }
+            }
+        }
+}
