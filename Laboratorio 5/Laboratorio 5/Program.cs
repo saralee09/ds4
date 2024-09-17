b4b58eb637bd905d2ded0f8e689ee787b482718c
SARA LEE <saragabyleegon2@mail.com>
2024-09-16 19:05:14 -0500

    Laboraatorio 5

---
+++ Laboratorio 5/Laboratorio 5/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] valores1 = new int[3, 7];
+        int[,,] valores2 = new int[3, 4, 2];
+
+        int[][] matriz = new int[3][];
+        for (int i = 0; i < matriz.Length; i++)
+        {
+            matriz[i] = new int[4];
+        }
+    }
+}
