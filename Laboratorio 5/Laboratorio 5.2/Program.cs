fc4ed93d3eb8daca1041a25da487af2d7881ee18
SARA LEE <saragabyleegon2@mail.com>
2024-09-16 19:32:46 -0500

    Laboratorio 5.2 Completo

--- Laboratorio 5/Laboratorio 5.2/Program.cs
+++ Laboratorio 5/Laboratorio 5.2/Program.cs
@@ -1,4 +1,4 @@
-﻿internal class Program
+﻿internal class Matriz
 {
         private int[,] mat;
         public void Ingresar()
@@ -14,5 +14,24 @@
                     mat[f, c] = int.Parse(linea);
                 }
             }
+        }
+        public void Imprimir()
+        {
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    Console.Write(mat[f, c] + " ");
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+       
+        static void Main(string[] args)
+        {
+            Matriz ma = new Matriz();
+            ma.Ingresar();
+            ma.Imprimir();
         }
 }
