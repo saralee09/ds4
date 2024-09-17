82c482fcbdac62a4d07857bd94b0a6864bf879be
SARA LEE <saragabyleegon2@mail.com>
2024-09-16 19:18:34 -0500

    Laboratorio 5.1

---
+++ Laboratorio 5/Laboratorio5.1/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class PruebaVector1
+{
+    private int[] sueldos;
+    public void Cargar()
+    {
+        sueldos = new int[6];
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("Ingrese sueldo del operario " + f + ": ");
+            String linea;
+            linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea);
+        }
+    }
+    public void Imprimir()
+    {
+         Console.Write("Los 5 sueldos de los aperarios \n");
+         for (int f = 1; f <= 5; f++)
+         {
+           Console.Write("[" + sueldos[f] + "] ");
+         }
+         Console.ReadKey();
+    }
+    static void Main(string[] args)
+    {
+        PruebaVector1 pv = new PruebaVector1();
+        pv.Cargar();
+        pv.Imprimir();
+    }
+}      
