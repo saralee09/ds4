6e388237dac16029c389e4ec876a38a1fd1636d3
SARA LEE <saragabyleegon2@mail.com>
2024-09-17 20:49:32 -0500

    Laboratorio 6.3

---
+++ Laboratorio6/Laboratorio6.3/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] MyNumbers = { 1, 2, 3 };
+            Console.WriteLine(MyNumbers[10]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("algo salio mal, valide el indice del arreglo");
+        }
+        finally
+        {
+            Console.WriteLine("Continuacion de la aplicacion lueo del bloque try/catch");
+        }
+    }
+}
