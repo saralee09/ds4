1af0637208afdaf13958ce8eab73eb7ebef6508b
SARA LEE <saragabyleegon2@mail.com>
2024-09-17 20:43:37 -0500

    Laboratorio 6.2

---
+++ Laboratorio6/Laboratorio6.2/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("digite el numero deseado: ");
+
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("No se ha introducido un digito valido ");
+            num = -1;
+        }
+        catch(OverflowException ex)
+        {
+            Console.WriteLine("El numero introducido es muy grande");
+            num = -1;
+        }
+        Console.WriteLine(num);
+    }
+}
