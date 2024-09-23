6edeeac7b95d6c090d3daff3a9bdf8f489fb3095
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 18:18:21 -0500

    Cuenta

---
+++ Laboatorio8/Laboratorio 9/Cuenta.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Laboratorio8
+{
+    public class Cuenta
+    {
+        private string idCuenta;
+
+        public Cuenta(string prmtIdCuenta)
+        {
+            this.idCuenta = prmtIdCuenta;
+            System.Console.WriteLine("Constructor Clase Base para cuenta {0}", prmtIdCuenta);
+        }
+
+        public virtual void CalcularIntereses()
+        {
+            System.Console.WriteLine("Cuenta.CalcularIntereses() efectuado para la cuenta {0}", this.idCuenta);
+        }
+
+        public string getIdCuenta()
+        {
+            return this.idCuenta;
+        }
+    }
+}
