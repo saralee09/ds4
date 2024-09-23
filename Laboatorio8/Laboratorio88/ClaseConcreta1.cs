2d4836a3f63711441fef6898331736cb914d1c7a
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 18:46:12 -0500

    Lab 8.8

---
+++ Laboatorio8/Laboratorio88/ClaseConcreta1.cs
@@ -0,0 +1,21 @@
+﻿using Laboratorio88;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8
+{
+    internal class ClaseConcreta1 : ClaseAbstracta
+    {
+        protected override string tomarValor()
+        {
+            return "ClaseConcreta1";
+        }
+        public override string prefixValor(string prefix)
+        {
+            return $"{prefix}ClaseConcreta1";
+        }
+    }
+}
--- Laboatorio8/Laboratorio88/Program.cs
+++ Laboatorio8/Laboratorio88/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Laboratorio88;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ClaseConcreta1 concreta1 = new ClaseConcreta1();
+            concreta1.printOut();
+            Console.WriteLine(concreta1.prefixValor("ES_"));
+
+            ClaseConcreta2 concreta2 = new ClaseConcreta2();
+            concreta2.printOut();
+            Console.WriteLine(concreta2.prefixValor("ES_"));
+        }
+    }
+}
