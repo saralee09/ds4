c71fb988ee72d3b2d46298fb8e736b1bad47b8c6
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 18:20:14 -0500

    Cuenta Corriente

---
+++ Laboatorio8/Laboratorio 9/CuentaCorriente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8
+{
+    public class CuentaCorriente : Cuenta
+    {
+        public CuentaCorriente(string prmtIdCuenta) : base(prmtIdCuenta)
+        {
+        }
+
+        public override void CalcularIntereses()
+        {
+            System.Console.WriteLine("CuentaAhorro.CalcularIntereses() efectuado para " + "la cuenta {0}", getIdCuenta());
+        }
+    }
+
+}
