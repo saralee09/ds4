f2b83c50eeee14c639ba82f5e7c63891e20f4f06
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 18:28:54 -0500

    Laboratorio 8.5

--- Laboatorio8/Laboratorio85/Program.cs
+++ Laboatorio8/Laboratorio85/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Coordenadas misCoords = new Coordenadas(10, 15);
+            misCoords.VerCoordenadas();
+        }
+    }
+}
