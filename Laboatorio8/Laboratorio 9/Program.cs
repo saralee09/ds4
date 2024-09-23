69c380f57bd3ba10f765d2caa8068c322a1994af
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 18:17:22 -0500

    Lab 8

---
+++ Laboatorio8/Laboratorio 9/Program.cs
@@ -0,0 +1,24 @@
+﻿using Laboratorio8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio82
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string CUENTA = "100";
+
+            Cuenta cuenta = new Cuenta(CUENTA);
+            CuentaCorriente cuentaCorriente = new CuentaCorriente(CUENTA);
+            CuentaAhorro cuentaAhorro = new CuentaAhorro(CUENTA);
+            cuenta.CalcularIntereses();
+            cuentaCorriente.CalcularIntereses();
+            cuentaAhorro.CalcularIntereses();
+        }
+    }
+}
