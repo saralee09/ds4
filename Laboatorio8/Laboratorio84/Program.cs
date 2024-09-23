77ad2ae5e42bd8fd9ba35902fbdc20e3281d34a5
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 18:26:06 -0500

    Lab 8.4

--- Laboatorio8/Laboratorio84/Program.cs
+++ Laboatorio8/Laboratorio84/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Empleado empleado = new Empleado();
+            empleado.Nombre = "John Doe";
+            Console.WriteLine($"Nombre del empleado: {empleado.Nombre}");
+
+            CuentaBancaria cta = new CuentaBancaria();
+            cta.Saldo = 100;
+            Console.WriteLine($"El saldo del empleado: {cta.Saldo}");
+
+            Cobertura c = new Cobertura(5);
+            Console.WriteLine($"Con una cobertura de: {c.Radio}");
+        }
+    }
+}
