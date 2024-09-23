992807b316890e250c7273da81df593f4ff5cac8
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 18:00:37 -0500

    Lab7.1

---
+++ Laboratorio7/Laboratorio7/Program.cs
@@ -0,0 +1,14 @@
+﻿using Laboratorio71;
+namespace Laboratorio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+    }
+}
