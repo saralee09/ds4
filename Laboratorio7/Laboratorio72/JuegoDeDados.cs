e23f6570711eb40099ea6633fd8f7960c2438929
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 18:03:44 -0500

    Juego De Dados

---
+++ Laboratorio7/Laboratorio72/JuegoDeDados.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Laboratorio72
+{
+    internal class JuegoDeDados
+    {
+        private Dado dado1, dado2, dado3;
+        public JuegoDeDados()
+        {
+            dado1 = new Dado();
+            dado2 = new Dado();
+            dado3 = new Dado();
+        }
+        public void Jugar()
+        {
+            dado1.Tirar();
+            dado1.Imprimir();
+            dado2.Tirar();
+            dado2.Imprimir();
+            dado3.Tirar();
+            dado3.Imprimir();
+            if (dado1.RetornarValor() == dado2.RetornarValor() && dado1.RetornarValor() == dado3.RetornarValor())
+            {
+                Console.WriteLine("Ha ganado");
+            }
+            else
+            {
+                Console.WriteLine("Ha perdido");
+            }
+            Console.ReadKey();
+        }
+    }
+}
