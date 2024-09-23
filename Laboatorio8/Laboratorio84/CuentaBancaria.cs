470a73e17c9d38beeb04604bdf1bbc6f2eed7d10
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 18:25:06 -0500

    Cuenta Bancaria

--- Laboatorio8/Laboratorio84/CuentaBancaria.cs
+++ Laboatorio8/Laboratorio84/CuentaBancaria.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Laboratorio84
+namespace Laboratorio8
 {
-    internal class CuentaBancaria
+    public class CuentaBancaria
     {
+        private decimal saldo;
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+            set { if (value >= 0) saldo = value; else throw new ArgumentException("El saldo no puede ser negativo."); }
+        }
     }
 }
