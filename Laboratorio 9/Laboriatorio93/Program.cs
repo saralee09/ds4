7ae050fb501fb8125000b3e77bcb69b71f21fd0b
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 19:33:06 -0500

    Lab 9.3

--- Laboratorio 9/Laboriatorio93/Program.cs
+++ Laboratorio 9/Laboriatorio93/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Triangulo triangulo = new Triangulo();
+        triangulo.SolicitarDatos();
+    }
+}
