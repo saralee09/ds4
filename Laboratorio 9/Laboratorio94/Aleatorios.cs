8c4d564757e8c95927913ecf470b074d4fdefda1
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 19:35:34 -0500

    Lab 9.4

---
+++ Laboratorio 9/Laboratorio94/Aleatorios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Aleatorios
+{
+    private Random random;
+
+    public Aleatorios()
+    {
+        random = new Random();
+    }
+
+    public int GenerarNumeroEntre(int min, int max)
+    {
+        return random.Next(min, max + 1);
+    }
+
+    public int[] GenerarArregloNumerosEntre(int min, int max, int tamaño)
+    {
+        int[] arreglo = new int[tamaño];
+        for (int i = 0; i < tamaño; i++)
+        {
+            arreglo[i] = GenerarNumeroEntre(min, max);
+        }
+        return arreglo;
+    }
+}
--- Laboratorio 9/Laboratorio94/Program.cs
+++ Laboratorio 9/Laboratorio94/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Aleatorios aleatorios = new Aleatorios();
+
+        Console.WriteLine("Número aleatorio entre 5 y 10: " + aleatorios.GenerarNumeroEntre(5, 10));
+
+        Console.WriteLine("Arreglo de números aleatorios entre 1 y 50:");
+        int[] arreglo = aleatorios.GenerarArregloNumerosEntre(1, 50, 5);
+        foreach (int numero in arreglo)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
