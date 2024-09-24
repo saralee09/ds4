e6a4a5584ef88135687f1361dc0217d4120d5e3d
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 19:30:27 -0500

    Lab 9.2

---
+++ Laboratorio 9/Laboratorio92/Numeros.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Numeros
+{
+    public void RecorrerNumeros()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0 || i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
