0a7d70c4ebbcd3d478c29fa19227e2ebae896ce0
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 19:41:36 -0500

    Lab94 as reference

--- Laboratorio 9/Laboratorio94/Aleatorios.cs
+++ Laboratorio 9/Laboratorio94/Aleatorios.cs
@@ -1,26 +1,53 @@
 ﻿using System;
 
-public class Aleatorios
+namespace Lab94
 {
-    private Random random;
-
-    public Aleatorios()
+    public class Aleatorios
     {
-        random = new Random();
-    }
+        private Random random;
 
-    public int GenerarNumeroEntre(int min, int max)
-    {
-        return random.Next(min, max + 1);
-    }
-
-    public int[] GenerarArregloNumerosEntre(int min, int max, int tamaño)
-    {
-        int[] arreglo = new int[tamaño];
-        for (int i = 0; i < tamaño; i++)
+        public Aleatorios()
         {
-            arreglo[i] = GenerarNumeroEntre(min, max);
+            random = new Random();
         }
-        return arreglo;
+
+        public int GenerarNumeroEntre(int min, int max)
+        {
+            return random.Next(min, max + 1);
+        }
+
+        public int[] GenerarArregloNumerosEntre(int min, int max, int tamaño)
+        {
+            int[] arreglo = new int[tamaño];
+            for (int i = 0; i < tamaño; i++)
+            {
+                arreglo[i] = GenerarNumeroEntre(min, max);
+            }
+            return arreglo;
+        }
+
+        public int[] GenerarArregloNumerosNoRepetidos(int min, int max, int tamaño)
+        {
+            if (tamaño > (max - min + 1))
+            {
+                throw new ArgumentException("El rango no puede contener suficientes números únicos para el tamaño solicitado.");
+            }
+
+            int[] arreglo = new int[tamaño];
+            HashSet<int> numerosGenerados = new HashSet<int>();
+
+            int contador = 0;
+            while (contador < tamaño)
+            {
+                int numero = GenerarNumeroEntre(min, max);
+                if (!numerosGenerados.Contains(numero))
+                {
+                    numerosGenerados.Add(numero);
+                    arreglo[contador] = numero;
+                    contador++;
+                }
+            }
+            return arreglo;
+        }
     }
 }
--- Laboratorio 9/Laboratorio95/Program.cs
+++ Laboratorio 9/Laboratorio95/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Lab94;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Aleatorios aleatorios = new Aleatorios();
+
+        Console.WriteLine("Arreglo de números no repetidos entre 1 y 50:");
+        int[] arregloNoRepetidos = aleatorios.GenerarArregloNumerosNoRepetidos(1, 50, 10);
+        foreach (int numero in arregloNoRepetidos)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
