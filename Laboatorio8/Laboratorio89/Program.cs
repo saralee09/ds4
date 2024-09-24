e185c0c7474363bbe8d2ad491bc534ad02952308
SARA LEE <saragabyleegon2@mail.com>
2024-09-23 19:08:05 -0500

    Lab 8.9

--- Laboatorio8/Laboratorio89/Program.cs
+++ Laboatorio8/Laboratorio89/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Template temp1 = new Template();
+            temp1.ponerVariable("var1", "Valor 1");
+            temp1.ponerVariable("var2", "Valor 2");
+            temp1.ponerVariable("var3", "Valor 3");
+            temp1.verHtml("<br>Texto de Prueba</br>");
+        }
+    }
+}
---
+++ Laboatorio8/Laboratorio89/iTemplate.cs
@@ -0,0 +1,8 @@
+﻿namespace Laboratorio8
+{
+    interface iTemplate
+    {
+        public void ponerVariable(string nombre, string var);
+        public void verHtml(string template);
+    }
+}
