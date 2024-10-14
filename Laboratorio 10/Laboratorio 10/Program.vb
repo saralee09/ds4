3188a7bc5db247326cf20f3dc7f0f4f73cd168cd
SARA LEE <saragabyleegon2@mail.com>
2024-10-14 06:13:56 -0500

    lab11

--- Laboratorio 10/Laboratorio 10/Program.vb
+++
@@ -1,6 +0,0 @@
-Module Laboratorio10
-    Sub Main()
-        Console.WriteLine("Hola")
-        Console.WriteLine("Este es mi primer programa de consola")
-    End Sub
-End Module
---
+++ Laboratorio11/Laboratorio11/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClickThis.Click
+        lblHelloWorld.Text = "Hello World!"
+    End Sub
+End Class
