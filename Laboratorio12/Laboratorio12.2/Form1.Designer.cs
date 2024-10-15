5f1baac5a6a61da95222d04ad8d932999dfc72f5
SARA LEE <saragabyleegon2@mail.com>
2024-10-15 00:32:13 -0500

    lab12.2 Completo !!

--- Laboratorio12/Laboratorio12.2/Form1.Designer.cs
+++ Laboratorio12/Laboratorio12.2/Form1.Designer.cs
@@ -28,13 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.lblNota3 = new System.Windows.Forms.Label();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.btnProm = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(460, 92);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(57, 20);
+            this.txtNota1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(361, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nota Promedio";
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota1.Location = new System.Drawing.Point(312, 93);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(91, 19);
+            this.lblNota1.TabIndex = 2;
+            this.lblNota1.Text = "NOTA No. 1";
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota2.Location = new System.Drawing.Point(312, 123);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(91, 19);
+            this.lblNota2.TabIndex = 3;
+            this.lblNota2.Text = "NOTA No. 2";
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(460, 122);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(57, 20);
+            this.txtNota2.TabIndex = 4;
+            // 
+            // lblNota3
+            // 
+            this.lblNota3.AutoSize = true;
+            this.lblNota3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota3.Location = new System.Drawing.Point(312, 153);
+            this.lblNota3.Name = "lblNota3";
+            this.lblNota3.Size = new System.Drawing.Size(91, 19);
+            this.lblNota3.TabIndex = 5;
+            this.lblNota3.Text = "NOTA No. 3";
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(460, 154);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(57, 20);
+            this.txtNota3.TabIndex = 6;
+            // 
+            // btnProm
+            // 
+            this.btnProm.Location = new System.Drawing.Point(316, 194);
+            this.btnProm.Name = "btnProm";
+            this.btnProm.Size = new System.Drawing.Size(63, 23);
+            this.btnProm.TabIndex = 7;
+            this.btnProm.Text = "Promedio";
+            this.btnProm.UseVisualStyleBackColor = true;
+            this.btnProm.Click += new System.EventHandler(this.btnProm_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(385, 194);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(63, 23);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(454, 194);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(63, 23);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(312, 236);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(103, 19);
+            this.lblResultado.TabIndex = 10;
+            this.lblResultado.Text = "Nota Promedio";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(460, 237);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(57, 20);
+            this.txtResultado.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnProm);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.lblNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lblNota1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNota1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNota1;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.Label lblNota3;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Button btnProm;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtResultado;
     }
 }
 
--- Laboratorio12/Laboratorio12.2/Form1.cs
+++ Laboratorio12/Laboratorio12.2/Form1.cs
@@ -16,5 +16,38 @@ namespace Laboratorio12._2
         {
             InitializeComponent();
         }
+
+
+        private void btnProm_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double nota1 = Convert.ToDouble(txtNota1.Text);
+                double nota2 = Convert.ToDouble(txtNota2.Text);
+                double nota3 = Convert.ToDouble(txtNota3.Text);
+
+
+                double prom = (nota1+nota2+nota3)/3;
+
+                txtResultado.Text = prom.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Pro favor, inserta cantidades validas en los campos de notas");
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            txtNota1.Text = "";
+            txtNota2.Text = "";
+            txtNota3.Text = "";
+            txtResultado.Text = "";
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
