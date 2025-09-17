d832901877bd3ac5b9dc0c602ae24f9e38cdb9d7
sara lee local <sara.lee@utp.ac.pa>
2025-09-16 19:51:05 -0500

    fixing folder issue git

---
+++ Laboratorio 2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Laboraorio2
+{
+    private static void Main(string[] args)
+    {
+        Client client = new Client
+        {
+            FirstName = "su_nombre",
+            LastName = "su_apellido",
+            Age = 15,
+            ID = 1
+        };
+
+        Console.WriteLine(client.GetFullname());
+    }
+}
