eb6ca1d99e87c9e1c26265e0288e7d68146c7abf
sara lee local <sara.lee@utp.ac.pa>
2025-09-16 20:55:08 -0500

    Lab 3

--- Laboratorio 3/Program.cs
+++ Laboratorio 3/Program.cs
@@ -2,6 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int primerNumero, segundoNumero, suma;
+
+        Console.WriteLine("Introduce el primer número: ");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduce el segundo número: ");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        suma = primerNumero + segundoNumero;
+        Console.WriteLine("La suma de {0} y {1} es {2}", primerNumero, segundoNumero, suma);
     }
 }
