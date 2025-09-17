2428143bbd23da9e703f0193a3e5fdc8681f9d45
sara lee local <sara.lee@utp.ac.pa>
2025-09-16 20:14:14 -0500

    Accediendo a variable de clase o estatica

---
+++ Laboratorio 2/Laboratorio 21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboraorio21
+{
+    public class Program
+    {
+        public static void MAIN()
+        {
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+
+    public class MyClass
+    {
+        public static int Valor;
+    }
+
+}
