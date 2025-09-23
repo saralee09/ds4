be30188d876ad0ed3f16db3bbcbe10be73f2b59c
sara lee local <sara.lee@utp.ac.pa>
2025-09-22 19:11:08 -0500

    Lab 4.3

---
+++ Laboratorio 42/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        int fac = 1, n;
+        string linea;
+        Console.Write("Ingrese un numero entero:");
+        linea = Console.ReadLine();
+        n = int.Parse(linea);
+        for (int i = 1; i < n; i++)
+        {
+            fac = fac * i;
+        }
+
+        Console.Write("la factorial es: " + fac);
+        Console.ReadKey();
+    }
+}
---
+++ Laboratorio 43/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        int suma, cant, valor, promedio;
+        string linea;
+        suma = 0;
+        cant = 0;
+
+        do
+        {
+            Console.Write("ingrese un numero (0 para finalizar):");
+            linea = Console.ReadLine();
+            valor = int.Parse(linea);
+            if (valor != 0 )
+            {
+                suma = suma + valor;
+                cant++;
+
+            }
+        } while (valor  != 0);
+        if (cant != 0)
+        {
+            promedio = suma / cant;
+            Console.Write("el promedio de los valores ingresados es:");
+            Console.Write(promedio);
+
+        }
+        else
+        {
+            Console.Write("No se ingresaron Valores");
+        }
+        Console.ReadLine();
+    }
+}
