20f9368eeb072fc697279765e00540f91517ec60
sara lee local <sara.lee@utp.ac.pa>
2025-09-22 22:25:03 -0500

    Lab 5.3

--- Laboratorio 5/Laboratorio 51/obj/Debug/net8.0/Laboratorio 51.AssemblyInfo.cs
+++ Laboratorio 5/Laboratorio 51/obj/Debug/net8.0/Laboratorio 51.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 51")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798aaff16bfba4a4545a36c27f0abad4f841f457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8238195217431b9fb7593084019053a3c9dce4b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 51")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 51")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Laboratorio 5/Laboratorio 53/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] frutas = { "manzana", "plátano", "naranja" };
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
