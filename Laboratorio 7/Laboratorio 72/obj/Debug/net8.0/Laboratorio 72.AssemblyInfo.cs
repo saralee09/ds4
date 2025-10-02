32ea9a60d7525aaecd02c2338ebf78dbde89eab1
sara lee local <sara.lee@utp.ac.pa>
2025-10-01 19:05:01 -0500

    clase dado

--- Laboratorio 7/Laboratorio 72/obj/Debug/net8.0/Laboratorio 72.AssemblyInfo.cs
+++ Laboratorio 7/Laboratorio 72/obj/Debug/net8.0/Laboratorio 72.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 72")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e1060c9d30f07ee86acbb600538439f7a70608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8505df0f67dce6cd8096efa727cb8676fcd6e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 72")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 72")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
