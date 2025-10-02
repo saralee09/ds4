c3cec8622cc90bd20a99d93d1c886d81f0238941
sara lee local <sara.lee@utp.ac.pa>
2025-10-01 19:25:18 -0500

    Programa principal - Lab 8 completo

--- Laboratorio 8/Laboratorio 81/Program.cs
+++ Laboratorio 8/Laboratorio 81/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace Laboratorio8
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Trabajador p = new Trabajador("Josan", 22, "77588260-Z", 100000);
+            Console.WriteLine("Nombre=" + p.Nombre);
+            Console.WriteLine("Edad=" + p.Edad);
+            Console.WriteLine("NIF=" + p.NIF);
+            Console.WriteLine("Sueldo=" + p.Sueldo);
+            Console.ReadKey();
+        }
+    }
+}
