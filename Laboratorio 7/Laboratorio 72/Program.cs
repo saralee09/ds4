11eb9b5a5b5385bf70f350d0f478b465475354f4
sara lee local <sara.lee@utp.ac.pa>
2025-10-01 19:11:11 -0500

    Programa principal

--- Laboratorio 7/Laboratorio 72/Program.cs
+++ Laboratorio 7/Laboratorio 72/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Laboratorio72;
+namespace Laboratorio72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JuegoDeDados j = new JuegoDeDados();
+            j.Jugar();
+        }
+    }
+}
