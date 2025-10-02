b4d416aed7d343dfd2198dd5175cc8321e85168b
sara lee local <sara.lee@utp.ac.pa>
2025-10-01 19:21:00 -0500

    clase Persona

---
+++ Laboratorio 8/Laboratorio 81/Persona.cs
@@ -0,0 +1,21 @@
+﻿namespace Laboratorio8
+{
+    public class Persona
+    {
+        public string Nombre;
+        public int Edad;
+        public string NIF;
+
+        void Cumpleanos()
+        {
+            Edad++;
+        }
+
+        public Persona(string nombre, int edad, string nif)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            NIF = nif;
+        }
+    }
+}
