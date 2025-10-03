9c278d92ddd2bda25af3a3444ae495c2eac0849e
sara lee local <sara.lee@utp.ac.pa>
2025-10-02 21:18:24 -0500

    Clase Hijo

--- Laboratorio 87/Laboratorio 87/ClaseHijo.cs
+++ Laboratorio 87/Laboratorio 87/ClaseHijo.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Laboratorio_87
+namespace Laboratorio8
 {
-    internal class ClaseHijo
+    internal class ClaseHijo : ClaseBase
     {
     }
 }
