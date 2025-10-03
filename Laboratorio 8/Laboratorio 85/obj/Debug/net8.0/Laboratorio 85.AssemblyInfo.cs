7e205d0b4ad4a40b128e425ec335410b14d8826e
sara lee local <sara.lee@utp.ac.pa>
2025-10-02 21:21:36 -0500

    Abstracta

--- Laboratorio 8/Laboratorio 85/obj/Debug/net8.0/Laboratorio 85.AssemblyInfo.cs
+++ Laboratorio 8/Laboratorio 85/obj/Debug/net8.0/Laboratorio 85.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 85")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9118b913c9da6695f906bcd4a0a2f18321c265")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3dd85b12e5e5bd9ed364c5dc12cb4d150494fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 85")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 85")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Laboratorio 8/Laboratorio 88/ClaseAbstracta.cs
@@ -0,0 +1,12 @@
+﻿namespace Laboratorio8
+{
+    abstract class ClaseAbstracta
+    {
+        abstract protected string tomarValor();
+        abstract public string prefixValor(string prefix);
+        public void printOut()
+        {
+            Console.WriteLine(tomarValor());
+        }
+    }
+}
