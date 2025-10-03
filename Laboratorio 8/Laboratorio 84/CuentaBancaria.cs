ee11e822764abd5b7271ccf4442a246bb202a75d
sara lee local <sara.lee@utp.ac.pa>
2025-10-02 21:02:23 -0500

    Empleado

---
+++ Laboratorio 8/Laboratorio 84/CuentaBancaria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8
+{
+    public class CuentaBancaria
+    {
+        private decimal saldo;
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+            set { if (value >= 0) saldo = value; else throw new ArgumentException("El saldo no puede ser negativo."); }
+        }
+    }
+}
--- Laboratorio 8/obj/Debug/net8.0/Laboratorio 8.AssemblyInfo.cs
+++ Laboratorio 8/obj/Debug/net8.0/Laboratorio 8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83bf291351ad34e5e8c762dd135f4ca29a6892b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1449449eabf4268e84cd8656d9eb5aa44bc897f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
