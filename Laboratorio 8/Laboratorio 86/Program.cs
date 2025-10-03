8d9118b913c9da6695f906bcd4a0a2f18321c265
sara lee local <sara.lee@utp.ac.pa>
2025-10-02 21:15:42 -0500

    Completo 8.6

--- Laboratorio 8/Laboratorio 86/Program.cs
+++ Laboratorio 8/Laboratorio 86/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8
+{
+    internal class Programm
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Corrio la aplicacion");
+        }
+    }
+}
