63be7093c63a28d79d7100901efe151ab2fd158f
sara lee local <sara.lee@utp.ac.pa>
2025-10-13 19:43:15 -0500

    lab 9.1

---
+++ Laboraatorio 9/Program.cs
@@ -0,0 +1,15 @@
+﻿//Laboratorio 9
+//Cada problema corresponde con un sub-laboratorio 
+/*1. Pedir al usuario el precio de un producto (valor positivo) y la forma de pagar (efectivo o 
+tarjeta) si la forma de pago es mediante tarjeta, pedir el número de cuenta (facilitado por 
+el usuario, de 16 dígitos)
+2. Recorrer los números del 1 al 100. Mostrar los números pares o divisibles entre 3.
+3. Ingresar 3 datos y decir que clase de triangulo es. Para formar un triángulo hay que tener 
+en cuenta que la suma de sus dos lados inferiores tiene que ser mayor al lado superior
+4. Crear una clase llamada: Aleatorios
+a. Como atributo tendrá un Random.
+b. Tendrá los siguientes métodos, cuyos objetivos son:
+i. Generar un numero entre 2 números
+ii. Generar un arreglo de números entre dos números aleatorios
+5. Utilizar la clase anterior de Aleatorios
+a. Generar un arreglo de números no repetidos entre dos números */
