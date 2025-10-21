e9385f3ac1aeabd154b3261257311d34e988d190
sara lee local <sara.lee@utp.ac.pa>
2025-10-20 20:48:46 -0500

    Parcial 1 - Parte 2

---
+++ Parcial 1/Parcial_1/Problema1.cs
@@ -0,0 +1,46 @@
+﻿internal class Matriz
+{
+    private int[,] mat;
+
+    public void Ingresar()
+    {
+        int x = 10; // fila
+        int y = 10; // columna
+        mat = new int[x, y];
+
+        // Primera fila en aumento hasta el valor maximo
+        for (int c = 0; c < y; c++)
+        {
+            mat[0, c] = c + 1;
+        }
+
+        // Última fila en retroceso hasta 1
+        for (int c = 0; c < y; c++)
+        {
+            //lo que mas se me complico, (vertical maxima, c) es y(que seria el numero que elegimos de N - C que seria el indice actual del arreglo en horizontal
+            mat[x - 1, c] = y - c;
+        }
+
+       
+    }
+
+    public void Imprimir()
+    {
+        for (int f = 0; f < mat.GetLength(0); f++)
+        {
+            for (int c = 0; c < mat.GetLength(1); c++)
+            {
+                Console.Write(mat[f, c] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+
+    static void Main(string[] args)
+    {
+        Matriz ma = new Matriz();
+        ma.Ingresar();
+        ma.Imprimir();
+    }
+}
---
+++ Parcial 1/Parte2/Problema2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SucesionMatematica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cuantos términos quiere el usuario
+            Console.Write("Ingrese el número de términos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sumatoria = 0;
+
+            // Encabezado de tabla para que quede centreado
+            Console.WriteLine("\n-------------------------------------------");
+            Console.WriteLine($"{"Iteración",-10}{"Término",-12}{"Sumatoria",-12}");
+            Console.WriteLine("-------------------------------------------");
+
+            // Bucle principal
+            for (int i = 1; i <= n; i++)
+            {
+                // los signoa negativo y positivo
+                int signo = (i % 2 == 0) ? -1 : 1;
+
+                // Cálculo del término: signo( 2^i)
+                int termino = signo * (int)Math.Pow(2, i);
+
+                // la sumatoria
+                sumatoria += termino;
+
+                Console.WriteLine($"{i,-10}{termino,-12}{sumatoria,-12}");
+            }
+
+        }
+    }
+}
+
