490977f459ed7eeb4f78c48b3fdc92bbd8967a20
sara lee local <sara.lee@utp.ac.pa>
2025-10-25 19:41:39 -0500

    btnResta

--- Proyecto1/Proyecto_1/Form1.cs
+++ Proyecto1/Proyecto_1/Form1.cs
@@ -36,7 +36,12 @@ namespace Proyecto_1
 
         private void btnResta_Click(object sender, EventArgs e)
         {
-
+            if (txtPantalla.Text != "")
+            {
+                valor1 = double.Parse(txtPantalla.Text);
+                operacion = "-";
+                txtPantalla.Clear();
+            }
         }
 
         private void btnSuma_Click(object sender, EventArgs e)
