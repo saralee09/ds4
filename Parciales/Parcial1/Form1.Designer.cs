8fa28b0a46acbbddc717010ca5834f1dc7c51ba7
sara lee local <sara.lee@utp.ac.pa>
2025-10-27 18:58:13 -0500

    Yardas a metros, diseno

--- Parciales/Parcial1/Form1.Designer.cs
+++ Parciales/Parcial1/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.lbl_m2y = new System.Windows.Forms.Label();
             this.btn_m2y = new System.Windows.Forms.Button();
             this.txt_m2yans = new System.Windows.Forms.TextBox();
+            this.txt_y2mans = new System.Windows.Forms.TextBox();
+            this.btn_y2m = new System.Windows.Forms.Button();
+            this.lbl_y2m = new System.Windows.Forms.Label();
+            this.txt_y2m = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txt_m2y
@@ -66,11 +70,47 @@
             this.txt_m2yans.Size = new System.Drawing.Size(100, 20);
             this.txt_m2yans.TabIndex = 3;
             // 
+            // txt_y2mans
+            // 
+            this.txt_y2mans.Location = new System.Drawing.Point(300, 85);
+            this.txt_y2mans.Name = "txt_y2mans";
+            this.txt_y2mans.Size = new System.Drawing.Size(100, 20);
+            this.txt_y2mans.TabIndex = 7;
+            // 
+            // btn_y2m
+            // 
+            this.btn_y2m.Location = new System.Drawing.Point(218, 86);
+            this.btn_y2m.Name = "btn_y2m";
+            this.btn_y2m.Size = new System.Drawing.Size(75, 23);
+            this.btn_y2m.TabIndex = 6;
+            this.btn_y2m.Text = "->";
+            this.btn_y2m.UseVisualStyleBackColor = true;
+            // 
+            // lbl_y2m
+            // 
+            this.lbl_y2m.AutoSize = true;
+            this.lbl_y2m.Location = new System.Drawing.Point(22, 89);
+            this.lbl_y2m.Name = "lbl_y2m";
+            this.lbl_y2m.Size = new System.Drawing.Size(84, 13);
+            this.lbl_y2m.TabIndex = 5;
+            this.lbl_y2m.Text = "Yardas a Metros";
+            // 
+            // txt_y2m
+            // 
+            this.txt_y2m.Location = new System.Drawing.Point(112, 86);
+            this.txt_y2m.Name = "txt_y2m";
+            this.txt_y2m.Size = new System.Drawing.Size(100, 20);
+            this.txt_y2m.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_y2mans);
+            this.Controls.Add(this.btn_y2m);
+            this.Controls.Add(this.lbl_y2m);
+            this.Controls.Add(this.txt_y2m);
             this.Controls.Add(this.txt_m2yans);
             this.Controls.Add(this.btn_m2y);
             this.Controls.Add(this.lbl_m2y);
@@ -89,6 +129,10 @@
         private System.Windows.Forms.Label lbl_m2y;
         private System.Windows.Forms.Button btn_m2y;
         private System.Windows.Forms.TextBox txt_m2yans;
+        private System.Windows.Forms.TextBox txt_y2mans;
+        private System.Windows.Forms.Button btn_y2m;
+        private System.Windows.Forms.Label lbl_y2m;
+        private System.Windows.Forms.TextBox txt_y2m;
     }
 }
 
