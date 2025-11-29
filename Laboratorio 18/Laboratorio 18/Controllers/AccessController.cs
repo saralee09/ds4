0c4dd30bd688a46689fdec305ddc05fa9738c971
sara lee local <sara.lee@utp.ac.pa>
2025-11-28 21:14:00 -0500

    Ajax con JQuery

---
+++ Laboratorio 18/Laboratorio 18/Controllers/AccessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio_18.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Laboratorio 18/Laboratorio 18/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Laboratorio_18.MvcApplication" Language="C#" %>
