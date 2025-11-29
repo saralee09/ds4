fd25114eade6284e0325758b54519bb52a18fc08
sara lee local <sara.lee@utp.ac.pa>
2025-11-29 11:57:18 -0500

    atributo action

---
+++ Laboratorio 19/Laboratorio 19/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Laboratorio_19.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Laboratorio 19/Laboratorio 19/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Laboratorio_19.WebApiApplication" Language="C#" %>
