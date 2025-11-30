63e018b802b1c0864e6e5ee21a4d4bbd28b0608c
sara lee local <sara.lee@utp.ac.pa>
2025-11-29 20:42:16 -0500

    cambios en el layout

--- Laboratorio 20/Laboratorio 202/App_Start/RouteConfig.cs
+++ Laboratorio 20/Laboratorio 202/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Laboratorio_202
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Matrix", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- Laboratorio 20/Laboratorio 202/Controllers/HomeController.cs
+++ Laboratorio 20/Laboratorio 202/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace Laboratorio_202.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
